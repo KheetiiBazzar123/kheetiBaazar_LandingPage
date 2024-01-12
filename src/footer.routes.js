61bf4f70f4a15d1bd236c15b51c44ac2e4aba65c
nidhi <nidhi7485@gmail.com>
2024-01-12 23:19:54 +0530

    changed in slider

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -1,7 +1,7 @@
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import YouTubeIcon from "@mui/icons-material/YouTube";
+// import FacebookIcon from "@mui/icons-material/Facebook";
+// import TwitterIcon from "@mui/icons-material/Twitter";
+// import GitHubIcon from "@mui/icons-material/GitHub";
+// import YouTubeIcon from "@mui/icons-material/YouTube";
 
 import MKTypography from "components/MKTypography";
 
@@ -16,63 +16,11 @@ export default {
     image: logoCT,
     route: "/",
   },
-  socials: [
-    {
-      icon: <FacebookIcon />,
-      link: "https://www.facebook.com/CreativeTim/",
-    },
-    {
-      icon: <TwitterIcon />,
-      link: "https://twitter.com/creativetim",
-    },
-    {
-      icon: <GitHubIcon />,
-      link: "https://github.com/creativetimofficial",
-    },
-    {
-      icon: <YouTubeIcon />,
-      link: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
-    },
-  ],
-  menus: [
-    {
-      name: "company",
-      items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
-        { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
-        { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
-        { name: "blog", href: "https://www.creative-tim.com/blog" },
-      ],
-    },
-    {
-      name: "resources",
-      items: [
-        { name: "illustrations", href: "https://iradesign.io/" },
-        { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
-        { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
-      ],
-    },
-    {
-      name: "help & support",
-      items: [
-        { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
-        { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
-        { name: "custom development", href: "https://services.creative-tim.com/" },
-        { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
-      ],
-    },
-    {
-      name: "legal",
-      items: [
-        { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
-        { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
-        { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
-      ],
-    },
-  ],
+  socials: [],
+  menus: [],
   copyright: (
     <MKTypography variant="button" fontWeight="regular">
-      All rights reserved. Copyright &copy; {date} Material Kit by{" "}
+      All rights reserved. Copyright &copy; {date}{" "}
       <MKTypography
         component="a"
         href="https://www.creative-tim.com"
@@ -81,7 +29,7 @@ export default {
         variant="button"
         fontWeight="regular"
       >
-        Creative Tim
+        Kheetii BaZaar
       </MKTypography>
       .
     </MKTypography>
--- src/pages/Presentation/sections/SliderSection.js
+++ src/pages/Presentation/sections/SliderSection.js
@@ -4,54 +4,103 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 // Images
-import card1 from "../../../assets/imagesWeb/images/cardImages/card1.jpg";
-import card2 from "../../../assets/imagesWeb/images/cardImages/card2.jpg";
-import card3 from "../../../assets/imagesWeb/images/cardImages/dcard3.jpg";
+// import card1 from "../../../assets/imagesWeb/images/cardImages/card1.jpg";
+// import card2 from "../../../assets/imagesWeb/images/cardImages/card2.jpg";
+// import card3 from "../../../assets/imagesWeb/images/cardImages/dcard3.jpg";
 
 const SliderSection = () => {
+  const slides = [
+    {
+      link: "https://indieweb.social/@kendalmintcode",
+      imageUrl:
+        "https://images.pexels.com/photos/1198802/pexels-photo-1198802.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://k-tech.systems",
+      imageUrl:
+        "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://indieweb.social/@kendalmintcode",
+      imageUrl:
+        "https://images.pexels.com/photos/1198802/pexels-photo-1198802.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://indieweb.social/@kendalmintcode",
+      imageUrl:
+        "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://indieweb.social/@kendalmintcode",
+      imageUrl:
+        "https://images.pexels.com/photos/1198802/pexels-photo-1198802.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://k-tech.systems",
+      imageUrl:
+        "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://indieweb.social/@kendalmintcode",
+      imageUrl:
+        "https://images.pexels.com/photos/1198802/pexels-photo-1198802.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      link: "https://indieweb.social/@kendalmintcode",
+      imageUrl:
+        "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ];
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
+    speed: 1000,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
     autoplay: true,
-    autoplaySpeed: 3000,
-  };
-
-  const imageContainerStyle = {
-    height: "50%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-
-  const imageStyle = {
-    height: "400px",
-    objectFit: "cover",
-    width: "100%",
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    pauseOnDotsHover: false,
+    touchMove: true,
+    draggable: true,
   };
   return (
-    <div style={{ width: "80%", margin: "auto", height: "300px" }}>
-      <Slider {...settings}>
-        <div>
-          <div style={imageContainerStyle}>
-            <img src={card1} alt="Image 1" style={imageStyle} />
+    <div className="my-7 mx-4 box-border">
+      <Slider {...settings} className="rounded-lg">
+        {slides.map((slide, index) => (
+          <div key={index} className="w-80% md:w-1/2 lg:w-1/3 xl:w-1/4 inline-block rounded-e-xl">
+            <a
+              href={slide.link}
+              rel="noopener noreferrer"
+              className="block w-80% h-64 focus:outline-none focus:ring focus:border-blue-300"
+            >
+              <img
+                src={slide.imageUrl}
+                alt={slide.link}
+                className="w-auto h-auto object-cover rounded-xl transition-transform transform scale-100 hover:scale-105 mx-2 p-2"
+              />
+            </a>
           </div>
-        </div>
-        <div>
-          <div style={imageContainerStyle}>
-            <img src={card2} alt="Image 2" style={imageStyle} />
-          </div>
-        </div>
-        <div>
-          <div style={imageContainerStyle}>
-            <img src={card3} alt="Image 2" style={imageStyle} />
-          </div>
-        </div>
+        ))}
       </Slider>
     </div>
   );
 };
-
 export default SliderSection;
